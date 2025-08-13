695be867771b729e973914acffb63b1dd9a8b4e7
BhanuPrakash019 <chintusaj@gmail.com>
2025-08-13 23:51:48 +0530

    Update API base URL to Render backend

--- src/components/Results.js
+++ src/components/Results.js
@@ -10,7 +10,7 @@ const Results = () => {
 
   const fetchData = async () => {
     try {
-      const res = await axios.get(`http://localhost:8085/details/${hallTicketNumber}`);
+      const res = await axios.get(`https://backend-scorecardcheck.onrender.com/details/${hallTicketNumber}`);
       setStudent(res.data);
       setError("");
     } catch (err) {
